ab43d7590fa7e79e8eb7dccfeb2046a6abdf9856
IyadaSangchan <73695797+IyadaSangchan@users.noreply.github.com>
2020-11-13 17:33:31 +0700

    Update show (2).php

--- show (2).php
+++ show (2).php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'labdataa.mysql.database.azure.com', 'iyada@labdataa', 'Daoyak171044', 'ITFLab', guestbook);
+mysqli_real_connect($conn, 'labdataa.mysql.database.azure.com', 'iyada@labdataa', 'Daoyak171044', 'ITFLab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
