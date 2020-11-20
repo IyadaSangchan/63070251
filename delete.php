fde15b2c2dff6656e7415fb289a90c11d6223052
IyadaSangchan <73695797+IyadaSangchan@users.noreply.github.com>
2020-11-21 01:31:43 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,22 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'labdataa.mysql.database.azure.com', 'iyada@labdataa', 'Daoyak171044', 'ITFLab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$ID = $_GET['ID'];
+$sql = "delete from guestbook where ID = $ID";
+
+
+if (mysqli_query($conn, $sql)) {
+    header('location:show.php');
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+
+mysqli_close($conn);
+?>
